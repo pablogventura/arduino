7c20ed1937af25d0bd307f5df438baef7bd48447
Pablo Ventura <pablogventura@gmail.com>
2015-09-15 23:43:24 -0300

    probando los encoders, no anda

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,72 @@
 
 #include <AFMotor.h>
 #include <Arduino.h>
+#define VEL (5000)
 
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
+AF_DCMotor motorizq(1);
+AF_DCMotor motorder(2);
+int izq;
+int der;
+int vder;
+int vizq;
 
 void setup() {
 
+    Serial.begin(9600);
+    
+    
 
-  motor1.run(RELEASE);
-    motor1.run(BACKWARD);
-    motor1.setSpeed(255);
- 
-  motor2.run(RELEASE);
-    motor2.run(FORWARD);
-    motor2.setSpeed(255);  
+    motorizq.run(FORWARD);
+    motorizq.setSpeed(255);
+
+    motorder.run(FORWARD);
+    motorder.setSpeed(255);  
 }
 
 void loop() {  
+    delay(100);
+    pulseIn(9,LOW);
+    der = abs(pulseIn(9,HIGH));
+    
+    pulseIn(10,LOW);
+    izq= abs(pulseIn(10,HIGH));
+    Serial.print("Der: ");
+    Serial.println(der);
+    Serial.print("Izq: ");
+    Serial.println(izq);
+    Serial.print("VDer: ");
+    Serial.println(vder);
+    Serial.print("VIzq: ");
+    Serial.println(vizq);    
+    if (izq > VEL){
+        vizq=vizq-1;
+    }
+    if (izq < VEL){
+        vizq=vizq+1;
+    }
+    if (der > VEL){
+        vder=vder-1;
+    }
+    if (der < VEL){
+        vder=vder+1;
+    }
+    
+    if (vizq>255){
 
+        vizq=255;
+    }
+    if (vder>255){
 
+        vder=255;
+    }    
+    if (vizq<100){
+
+        vizq=100;
+    }
+    if (vder<100){
+
+        vder=100;
+    }
+    motorizq.setSpeed(vizq);
+    motorder.setSpeed(vder);  
 }
